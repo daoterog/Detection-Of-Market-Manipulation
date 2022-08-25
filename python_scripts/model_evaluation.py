df7f5b6a865e9207bc63e88f51773fbcca9b1aa9
Daniel Otero Gomez <doterog320@gmail.com>
2022-08-25 18:47:48 -0500

    Created functions to calculate number of samples

--- python_scripts/model_evaluation.py
+++ python_scripts/model_evaluation.py
@@ -4,6 +4,85 @@ Model evaluation module.
 
 import numpy as np
 
+import operator as op
+from functools import reduce
+
+
+def n_combined_r(n: int, r: int):
+
+    """Returns the number of combinations of n and r."""
+
+    r = min(r, n - r)
+    numer = reduce(op.mul, range(n, n - r, -1), 1)
+    denom = reduce(op.mul, range(1, r + 1), 1)
+    return numer // denom
+
+
+def number_of_samples(vc_dim: int, gen_error: float, train_error: float):
+
+    """Give the probably approximated correct learning guarantee for a vc dimension (of hypothesis
+    family), a generalization error, and a train error."""
+
+    return (np.log(vc_dim) + np.log(1 / train_error)) / gen_error
+
+
+def number_of_samples_decision_tree(
+    depth: int, gen_error: float, train_error: float, n_features: int
+):
+
+    """Give the probably approximated correct learning guarantee for a given depth, a generalization
+    error, a train error, and a number of features."""
+
+    return (
+        np.log(2)
+        * (
+            (np.power(2, depth) - 1) * (1 + np.log2(n_features))
+            + 1
+            + np.log(1 / train_error)
+        )
+        / (2 * (gen_error**2))
+    )
+
+
+def find_vc_dim(hypthesis_family: str, n_features: int, pol_degree: int = None):
+
+    """Returns vc dimension of a given hypothesis family and number of features."""
+
+    if hypthesis_family == "linear":
+        return n_features + 1
+    elif hypthesis_family == "polynomial":
+        if pol_degree is None:
+            raise ValueError("Polynomial degree not specified.")
+        return n_combined_r(n_features + pol_degree - 1, pol_degree)
+    elif hypthesis_family == "rbf":
+        return np.inf
+    else:
+        raise ValueError(
+            "Unknown hypothesis family. Available hypothesis families are decision_tree, linear, polynomial, and rbf."
+        )
+
+
+def get_number_of_samples(
+    hypthesis_family: str,
+    n_features: int,
+    gen_error: float,
+    train_error: float,
+    pol_degree: int = None,
+    depth: int = None,
+):
+
+    """Returns the number of samples needed to guarantee correct learning for a given hypothesis family,
+    number of features, generalization error, and train error."""
+
+    if hypthesis_family == "decision_tree":
+        if depth is None:
+            raise ValueError("Depth not specified.")
+        return number_of_samples_decision_tree(
+            depth, gen_error, train_error, n_features
+        )
+    return number_of_samples(
+        find_vc_dim(hypthesis_family, n_features, pol_degree), gen_error, train_error
+    )
 
 
 def evaluate_model(
@@ -73,4 +152,3 @@ def evaluate_classifiers(
         )
 
     return classifiers_errors
-
